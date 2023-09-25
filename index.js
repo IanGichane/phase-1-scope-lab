009a021de93c5aa621d41093ccc953c10bea457c
Ian <gichaneian@gmail.com>
2023-09-25 22:02:03 +0300

    Completed assignment..Noted that the console.log outputs the expected result but the test does not

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+
+}
+console.log(customerName)
+
+
+let bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+  
+}
+setBestCustomer()
+console.log(bestCustomer)
+
+function overwriteBestCustomer(bestbestCustomer){
+   bestCustomer=bestbestCustomer
+}
+overwriteBestCustomer('maybe bob')
+console.log(bestCustomer)
+const leastFavoriteCustomer= 'maasai'
+function changeLeastFavoriteCustomer(updateCustomer){
+   leastFavoriteCustomer = updateCustomer
+}
+changeLeastFavoriteCustomer('Loso')
+console.log(leastFavoriteCustomer)
